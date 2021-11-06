cf19fe652c4705320068bfe5bd9b7fbd61766249
Erik Wegner <E_Wegner@web.de>
2021-11-06 21:44:47 +0100

    Second state

--- states/states.go
+++ states/states.go
@@ -3,19 +3,24 @@ package states
 import "github.com/qmuntal/stateless"
 
 const (
-	triggerInitMaintenance = "InitMaintenance"
+	triggerInitMaintenance          = "InitMaintenance"
+	triggerWaitingForTaskACompleted = "WaitingForTaskACompleted"
 )
 
 const (
-	stateActive = "Active"
-	stateCreateTaskA = "WaitingForTaskA"
+	stateActive                   = "Active"
+	stateCreateTaskA              = "WaitingForTaskA"
+	stateWaitingForTaskACompleted = "WaitingForTaskACompleted"
 )
 
 func GetMachine() *stateless.StateMachine {
 	machine := stateless.NewStateMachine(stateActive)
 
 	machine.Configure(stateActive).
-	 	Permit(triggerInitMaintenance, stateCreateTaskA)
+		Permit(triggerInitMaintenance, stateCreateTaskA)
+
+	machine.Configure(stateCreateTaskA).
+		Permit(triggerWaitingForTaskACompleted, stateWaitingForTaskACompleted)
 
 	return machine
 }
--- states/states_test.go
+++ states/states_test.go
@@ -2,11 +2,13 @@ package states
 
 import (
 	"testing"
+
+	"github.com/qmuntal/stateless"
 )
 
 // Transition from _active_ to _createTaskA_
 func TestFromActiveToCreateTaskA(t *testing.T) {
-	m := GetMachine()
+	m := machineAtActive()
 
 	err := m.Fire(triggerInitMaintenance)
 	if err != nil {
@@ -17,4 +19,29 @@ func TestFromActiveToCreateTaskA(t *testing.T) {
 	if finalState != stateCreateTaskA {
 		t.Errorf("want:%s\ngot:%s", stateCreateTaskA, finalState)
 	}
+}
+
+// Transition from _createTaskA_ to _WaingForTaskACompleted_
+func TestFromCreateTaskAToWaitingForTaskACompleted(t *testing.T) {
+	m := machineAtCreateTaskA()
+
+	err := m.Fire(triggerWaitingForTaskACompleted)
+	if err != nil {
+		t.Errorf("Got unexpected error: %s", err)
+	}
+
+	finalState := m.MustState()
+	if finalState != stateWaitingForTaskACompleted {
+		t.Errorf("want:%s\ngot:%s", stateCreateTaskA, finalState)
+	}
+}
+
+func machineAtActive() *stateless.StateMachine {
+	return GetMachine()
+}
+
+func machineAtCreateTaskA() *stateless.StateMachine {
+	m := machineAtActive()
+	m.Fire(triggerInitMaintenance)
+	return m
 }
